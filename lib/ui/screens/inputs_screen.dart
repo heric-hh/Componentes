1d99ac3b14afac333c5fe68172d565e3e868b86c
heric-hh <erhh.015@gmail.com>
2023-03-27 14:01:18 -0600

    Añadiendo inputs con funcionalidad

--- lib/ui/screens/inputs_screen.dart
+++ lib/ui/screens/inputs_screen.dart
@@ -18,21 +18,74 @@ class _InputsScreenState extends State<InputsScreen> {
       body: ListView(
         padding: EdgeInsets.symmetric(horizontal: 10, vertical: 20),
         children: [
-          _crearEntrada(),
+          _crearEntradaNombre(),
+          _crearEntradaPassword(),
         ],
       ),
     );
   }
   
-  Widget _crearEntrada() {
+  Widget _crearEntradaNombre() {
   return TextField(
     autofocus: true,
     textCapitalization: TextCapitalization.words,
+    textAlign: TextAlign.center,
+    style: const TextStyle(
+      color: Colors.indigo,
+      fontWeight: FontWeight.bold,
+    ),
+    cursorColor: Colors.red,
+    cursorRadius: const Radius.circular(16),
+    cursorWidth: 5,
+    //maxLength: 4,
+    maxLines: null,
     decoration: InputDecoration(
       border: OutlineInputBorder(
-        borderRadius: BorderRadius.circular(20)
+        borderRadius: BorderRadius.circular(20),
       ),
+      hintText: "Escriba su nombre por favor",
+      labelText: "Nombre: ",
+      prefixIcon: const Icon(Icons.person),
+      helperText: "",
+      //prefix: const CircularProgressIndicator(),
+      icon: Icon(Icons.person),
+      iconColor: Colors.indigo
     ),
+    onChanged: (valor) {
+     _userName = valor;
+     print(_userName);
+    },
+  );
+}
+
+Widget _crearEntradaPassword() {
+  return TextField(
+    autofocus: true,
+    textCapitalization: TextCapitalization.words,
+    textAlign: TextAlign.center,
+    style: const TextStyle(
+      color: Colors.indigo,
+      fontWeight: FontWeight.bold,
+    ),
+    cursorColor: Colors.red,
+    cursorRadius: const Radius.circular(16),
+    cursorWidth: 5,
+    maxLength: 8,
+    // maxLines: null,
+    decoration: InputDecoration(
+      border: OutlineInputBorder(
+        borderRadius: BorderRadius.circular(20),
+      ),
+      hintText: "Escriba su contraseña por favor",
+      labelText: "Password: ",
+      prefixIcon: const Icon(Icons.password),
+      helperText: "Escribe tu contraseña",
+      //prefix: const CircularProgressIndicator(),
+      icon: Icon(Icons.person),
+      iconColor: Colors.indigo
+    ),
+    obscureText: true,
+    keyboardType: TextInputType.number,
     onChanged: (valor) {
      _userName = valor;
      print(_userName);
